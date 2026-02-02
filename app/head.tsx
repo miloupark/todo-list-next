9e21f0a91b4cbacb2917a0934c4cc1beda4f1f55
hyebinpark <hoonywhitesocks@gmail.com>
2026-02-02 14:55:07 +0900

    chore: head 설정 분리

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <link
+      rel='stylesheet'
+      type='text/css'
+      href='https://cdn.jsdelivr.net/gh/moonspam/NanumSquare@2.0/nanumsquare.css'
+    ></link>
+  );
+}
