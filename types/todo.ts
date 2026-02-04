e76d92329d9732903ea455bda440862907c0ac7d
hyebinpark <hoonywhitesocks@gmail.com>
2026-02-04 22:39:19 +0900

    chore: Todo 관련 타입 수정

--- types/todo.ts
+++ types/todo.ts
@@ -1,31 +1,26 @@
-// Todo 목록 조회
 export type TodoListItem = {
-  id: string;
+  id: number;
   name: string;
   isCompleted: boolean;
 };
 
-// Todo 상세 조회
 export type TodoItem = {
-  id: string;
+  id: number;
   tenantId: string;
   name: string;
   memo: string;
-  imageUrl: string | null;
+  imageUrl: string;
   isCompleted: boolean;
 };
 
-// Todo 항목 수정 요청 타입
 export type TodoItemPatch = Partial<
   Pick<TodoItem, "name" | "memo" | "imageUrl" | "isCompleted">
 >;
 
-// Todo 항목 삭제 API 응답 타입
 export type DeleteResponse = {
   message: string;
 };
 
-// 이미지 업로드 API 응답 타입
 export type ImageUploadResponse = {
-  url: string;
+  imageUrl: string;
 };
